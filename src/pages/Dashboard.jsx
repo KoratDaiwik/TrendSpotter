5e75dff6ff1d08715032bdc89466ea9e7bd095f1
Daiwik Korat <daiwikkorat28@gmail.com>
2025-04-06 12:53:07 +0530

    Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -25,7 +25,7 @@ const Dashboard = () => {
   useEffect(() => {
     const fetchDashboardData = async () => {
       try {
-        const response = await axios.get("http://localhost:5000/dashboard");
+        const response = await axios.get("https://backendtrend-1.onrender.com");
         setData(response.data);
       } catch (err) {
         setError(err.response?.data?.error || "Failed to load dashboard data");
